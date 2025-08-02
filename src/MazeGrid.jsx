18e454fbd3e140c3eac223d24bdbd900239e25d0
anask601 <anask60155@gmail.com>
2025-08-02 17:29:08 +0530

    Add maze structure and update layout in MazeGrid component

--- src/MazeGrid.jsx
+++ src/MazeGrid.jsx
@@ -4,9 +4,17 @@ import viteLogo from "/vite.svg";
 import "./App.css";
 
 function MazeGrid() {
+  let maze = [
+    ["wall", "wall", "wall", "wall", "wall"],
+    ["start", "path", "path", "wall", "wall"],
+    ["wall", "path", "path", "wall", "wall"],
+    ["wall", "wall", "path", "path", "end"],
+  ];
   return (
     <>
-      <div className={"cell"}></div>
+      <div className="row">
+        <div className={"cell"}></div>
+      </div>
     </>
   );
 }
